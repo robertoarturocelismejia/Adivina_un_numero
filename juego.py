b22706c0c0a97e269b63b6e0cbf3355946cbc83b
robertoarturocelismejia <robertoarturocelismejia@gmail.com>
2025-04-12 08:46:48 -0500

    se añade módulo juego con la lógica del juego

--- juego.py
+++ juego.py
@@ -1,1 +1,22 @@
-print "hola mundo"
+# juego.py
+from utils import generar_numero_aleatorio
+def jugar():
+ print("Estoy pensando en un número del 1 al 10. ¿Puedes adivinarlo?")
+ numero_secreto = generar_numero_aleatorio(1, 10)
+ intentos = 0
+ while True:
+ respuesta = input("Ingresa tu número: ")
+ if not respuesta.isdigit():
+ print("Por favor, ingresa un valor numérico.")
+ continue
+ numero_ingresado = int(respuesta)
+ intentos += 1
+ if numero_ingresado < numero_secreto:
+ print("Demasiado bajo, intenta de nuevo.\n")
+ elif numero_ingresado > numero_secreto:
+ print("Demasiado alto, intenta de nuevo.\n")
+ else:
+ print(f"¡Felicidades! Adivinaste el número en {intentos} intento(s).")
+ break
+def despedida():
+ print("\nGracias por jugar. ¡Hasta la próxima!")
