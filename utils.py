98761c0b925c764703d77be4effd3622d1ecdbef
robertoarturocelismejia <robertoarturocelismejia@gmail.com>
2025-04-12 08:51:14 -0500

    se añade módulo utils con funciones de utilidad

---
+++ utils.py
@@ -0,0 +1,8 @@
+# utils.py
+import random
+def generar_numero_aleatorio(minimo, maximo):
+return random.randint(minimo, maximo)
+def mostrar_bienvenida():
+print("=======================================")
+print(" ¡Bienvenido al juego de Adivina el Número! ")
+print("=======================================\n")
